14604a9fd0a4688751ebd93516fb85f386055d66
Luke Engelhardt <luke@securitymetrics.com>
2015-08-03 14:09:56 -0600

    saving work

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,37 @@
-var app = angular.module('nbaRoutes');
+var app = angular.module('nbaRoutes').controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+    $scope.teamData = teamData;
+    $scope.newGame = {};
+    $scope.showNewGameForm = false;
+
+    $scope.toggleNewGameForm = function() {
+        $scope.showNewGameForm = !$scope.showNewGameForm;
+    };
+
+    if ($routeParams.team === 'utahjazz') {
+            $scope.homeTeam = 'Utah Jazz';
+            $scope.logoPath = 'images/jazz-logo.png';
+    } else if ($routeParams.team === 'losangeleslakers') {
+            $scope.homeTeam = 'Los Angeles Lakers';
+            $scope.logoPath = 'images/lakers-logo.png';
+    } else if ($routeParams.team === 'miamiheat') {
+            $scope.homeTeam = 'Miami Heat';
+            $scope.logoPath = 'images/heat-logo.png';
+    }
+
+    $scope.submitGame = function() {
+        $scope.newGame.homeTeam = $scope.newGame.homeTeam.split(' ').join('').toLowerCase()
+        teamService.addNewGame($scope.newGame)
+            .then(function() {
+            teamService.getTeamData($scope.newGame.homeTeam)
+                .then(function(result) {
+                    $scope.teamData = result;
+                    $scope.newGame = {};
+                    $scope.showNewGameForm = false;
+                })
+        })
+    }
+
+
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,43 @@
-var app = angular.module('nbaRoutes');
+var app = angular.module('nbaRoutes').service('teamService', function($http, $q){
 
-app.service('teamService', function($http, $q){
+    this.addNewGame = function(gameObj) {
+        var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+        if (parseInt('gameObj.homeTeamScore') > parseInt('gameObj.opponentScore')) {
+            gameObj.won = true;
+        } else {
+            gameObj.won = false;
+        }
+
+        return $http({
+            method: 'POST',
+            url: url,
+            data: gameObj
+        });
+    };
+
+    this.getTeamData = function(team) {
+        var dfd = $q.defer();
+        var url = 'https://api.parse.com/1/classes/' + team;
+        $http({
+            method: 'GET',
+            url: url
+        }).then(function(data) {
+            var results = data.data.results;
+            var wins = 0;
+            var losses = 0;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].won) {
+                    wins ++;
+                } else if (results[i].won === false) {
+                    losses ++;
+                }
+            }
+            results['wins'] = wins;
+            results['losses'] = losses;
+            dfd.resolve(results);
+            console.log(results);
+        });
+        return dfd.promise;
+    }
 
 });
